a49c739deb052c82c6071e389ab81c7bc14abdbf
Florian Schneider <florianschneider@Mac.fritz.box>
2025-11-10 13:41:40 +0100

    seo push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://www.lsd-berlin.de',
-  outDir: './dist',
-  output: 'static', // ✅ Wichtig! Keine "server"-Angabe
+  site: "https://www.lsd-berlin.de",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
