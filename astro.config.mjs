ed6870aadc19807631eefe4b117f793e47f9c21f
Florian Schneider <florianschneider@MacBook-Air-von-Florian-2.fritz.box>
2025-11-12 15:04:00 +0100

    rave

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import node from "@astrojs/node";
 export default defineConfig({
   site: "https://www.lsd-berlin.de",
   outDir: "./dist",
-  output: "server",            // ⬅️ statt "static"
+  output: "server", // ⬅️ statt "static"
   adapter: node({ mode: "standalone" }), // ⬅️ Node-Adapter aktivieren
   server: {
     host: true,
